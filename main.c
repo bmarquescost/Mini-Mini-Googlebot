07a4eb7960f430a9f3cf4a1cf5607c215212956f
bmarquescost <b.marquescost@gmail.com>
2020-10-12 16:25:56 -0300

    Adicionado comentários na main, menu e util

--- main.c
+++ main.c
@@ -1,3 +1,16 @@
+/*
+    ICMC - Instituto de Ciências Matemáticas e da Computação
+    BCC  - Bacharelado em Ciências de Computação
+    
+    SCC0202 - ALGORITMOS E ESTRUTURAS DE DADOS I
+    Docente: Rudinei Goulart 
+
+    Aluno: Bernardo Marques Costa                    Número USP: 11795551
+    Aluno: Fernando Henrique Paes Generich           Número USP: 11795342
+
+    MINI GOOGLEBOT 
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,9 +21,12 @@
 #include "listaencadeada.h"
 
 int main(int argc, char *argv[]){
+    // imprimindo o cabeçalho do programa
     printf("______________________________________________________________\n");
     printf("\n\t\t\tGOOGLEBOT\n");
     printf("______________________________________________________________\n\n");
+    
+    // Leitura do arquivo padrão "googlebot.txt"
     printf("Obtendo dados do arquivo padrão inicial ...\n\n");    
     
     LISTA *lista_sites;
@@ -18,8 +34,10 @@ int main(int argc, char *argv[]){
     preenche_lista_dados_csv(lista_sites, ARQUIVO_GOOGLEBOT);
     if(lista_vazia(lista_sites) == FALSE) printf("Leitura de dados de Googlebot.txt bem sucedida.\n");
     
+    // Executando o programa de interação com o usuário
     executa_programa(lista_sites);
 
+    // Liberando a memória heap utilizada pelo programa.
     lista_deletar(&lista_sites);
 
     return 0;
